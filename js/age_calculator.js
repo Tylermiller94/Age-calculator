92a0d395c3615d4cf9f5346d01eba1b246e7fdc2
tylermiller94 <tylermiller94@gmail.com>
2017-10-24 02:08:54 -0700

    non-working build

--- js/age_calculator.js
+++ js/age_calculator.js
@@ -3,13 +3,35 @@ class AgeCalculator {
     this.age = age
   }
 
- ageSeconds(age) {
-  return age * 31536000;
- }
+  ageSeconds(age) {
+    return age * 31536000;
+  }
+
+  secondsByDates(mostCurentDate, otherDate){
+    let date1 = new Date(mostCurentDate);
+    let date2 = new Date(otherDate);
+    let difference = date1.getUTCFullYear() - date2.getUTCFullYear();
+    return this.ageSeconds(difference);
+  }
+
+  yearsOnMercury(age){
+    let earthDays = 365 * age;
+    return parseInt(earthDays / 88);
+  }
+
+  yearsOnVenus(age){
+    let earthDays = 365 * age;
+    return parseInt(earthDays / 243);
+  }
+
+  yearsOnMars(age){
+    let earthDays = 365 * age;
+    return parseInt(earthDays / 686)
+  }
 }
 
 
 
-
-
-exports.ageModule = AgeCalculator;
+//
+// for testing jasmine
+// exports.ageModule = AgeCalculator;
--- spec/age_calculator_spec.js
+++ spec/age_calculator_spec.js
@@ -1,15 +1,19 @@
-// import {AgeCalculator} from './../js/age_calculator.js';
+import {AgeCalculator} from './../js/age_calculator.js';
+// used for jasmine testing
+// var AgeCalculator = require('./../js/age_calculator.js').ageModule;
 
-var AgeCalculator = require('./../js/age_calculator.js').ageModule;
+describe('AgeCalculator', function() {
+  var calculateAge;
 
-describe('AgeCalculator', function(){
-  let Calculator;
-  beforeEach(function(){
-  Calculator = new AgeCalculator(22)
+  beforeEach(function() {
+    calculateAge = new AgeCalculator()
   });
-
 
   it("should return age in seconds", function(){
     expect(Calculator.ageSeconds(22)).toEqual(693792000)
+  });
+
+  it("should return dates difference in seconds", function(){
+  expect(calculateAge.secondsByDates('10-13-2017', '11-02-1994')).toEqual(693792000)
   });
 });
